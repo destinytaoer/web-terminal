0eefb12994c046d20b081a7b8fcc451b46e18ebe
destiny <1848765519@qq.com>
2023-04-21 20:56:24 +0800

    feat(node-pty): 实现前后端消息转换, 增加 resize 和 heartbeat 能力

--- apps/client/src/modules/node-pty/service/useTerminal.ts
+++ apps/client/src/modules/node-pty/service/useTerminal.ts
@@ -1,104 +1,84 @@
-import { useEffect, useRef } from "react";
-import { useCreation } from "ahooks";
-import { throttle } from "lodash";
-import { v4 as uuid } from "uuid";
-import { WebTerminal } from "@/core/WebTerminal";
+import { useEffect, useRef } from 'react'
+import { useCreation } from 'ahooks'
+import { throttle } from 'lodash'
+import { v4 as uuid } from 'uuid'
+import { WebTerminal } from '@/core/WebTerminal'
+import { generateMessage, processMessageFromServer } from './config'
 
-const url = "ws://127.0.0.1:3001/node-pty";
+const url = 'ws://127.0.0.1:3001/node-pty'
 export const useTerminal = () => {
-  const terminalEl = useRef<HTMLDivElement>(null);
+  const terminalEl = useRef<HTMLDivElement>(null)
   const terminal = useCreation(() => {
-    return new WebTerminal();
-  }, []);
+    return new WebTerminal({
+      fontSize: 20,
+    })
+  }, [])
 
   // terminal 初始化
   useEffect(() => {
     if (terminalEl.current) {
-      terminal.init(terminalEl.current);
+      terminal.init(terminalEl.current)
     }
     return () => {
-      terminal.destroy();
-    };
-  }, []);
+      terminal.destroy()
+    }
+  }, [])
 
   useEffect(() => {
     if (url) {
-      const id = uuid();
-      const cols = terminal.cols;
-      const rows = terminal.rows;
-      const urlWithQuery = `${url}?id=${id}&cols=${cols}&rows=${rows}`;
+      const id = uuid()
+      const cols = terminal.cols
+      const rows = terminal.rows
+      const urlWithQuery = `${url}?id=${id}&cols=${cols}&rows=${rows}`
 
-      terminal.clear();
+      terminal.clear()
 
       const socket = terminal.connectSocket(urlWithQuery, [], {
-        sendMessageMapper: (type, data) => {
-          switch (type) {
-            case "resize":
-              const { cols, rows } = data as any;
-              const msg = JSON.stringify({
-                Width: cols,
-                Height: rows,
-              });
-              return JSON.stringify({ type: "resize", content: msg });
-            case "data":
-              const input = data as string;
-              return input;
-            case "binary":
-              break;
-            case "heartbeat":
-              return "";
-          }
-          return "";
-        },
-        onmessageMapper: (data) => {
-          if (typeof data === "string") {
-            return data;
-          }
-          return "";
-        },
-      });
+        sendMessageMapper: generateMessage,
+        onmessageMapper: processMessageFromServer,
+      })
 
-      let timer: number | undefined;
-      socket.addListener("open", () => {
+      let timer: number | undefined
+      socket.addListener('open', () => {
         // 在下一个事件循环中执行, 相当于 nextTick
         // Promise.resolve().then(() => {
         //   const { rows, cols } = terminal
         //   socket?.sendMessage('resize', { cols, rows })
         // })
 
-        terminal.focus();
+        terminal.focus()
 
         timer = window.setInterval(function () {
-          socket?.sendMessage("heartbeat");
-        }, 30 * 1000);
-      });
+          socket?.sendMessage('heartbeat')
+        }, 30 * 1000)
+      })
 
-      socket.addListener("error", (e) => {
-        terminal.write("Connect Error.");
-      });
+      socket.addListener('error', (e) => {
+        terminal.write('Connect Error.')
+      })
 
-      socket.addListener("close", (e) => {
-        const { code, reason, wasClean } = e;
-        terminal.write("disconnect.");
-        if (timer) clearInterval(timer);
-        terminal.destroySocket(false);
-      });
+      socket.addListener('close', (e) => {
+        const { code, reason, wasClean } = e
+        terminal.write('disconnect.')
+        if (timer) clearInterval(timer)
+        terminal.destroySocket(false)
+      })
 
       const resizeListener = terminal.onResize(
         throttle(({ cols, rows }) => {
-          socket?.sendMessage("resize", { cols, rows });
-        }, 300)
-      );
+          socket?.sendMessage('resize', { cols, rows })
+        }, 300),
+      )
 
       return () => {
-        if (timer) clearInterval(timer);
-        resizeListener.dispose();
-        terminal.destroySocket(true);
-      };
+        if (timer) clearInterval(timer)
+        resizeListener.dispose()
+        terminal.destroySocket(true)
+      }
     }
-  }, [url]);
+  }, [url])
 
   return {
     terminalEl,
-  };
-};
+  }
+}
--- apps/server/services/pty-ws.js
+++ apps/server/services/pty-ws.js
@@ -1,85 +1,125 @@
-const { WebSocketServer, OPEN } = require("ws");
-const qs = require("querystring");
-const Terminal = require("../utils/pty");
+const { WebSocketServer, OPEN } = require('ws')
+const qs = require('querystring')
+const Terminal = require('../utils/pty')
 
 class PtyWs {
-  online;
-  ws;
+  online
+  ws
 
-  terminals = {};
+  terminals = {}
 
   constructor(server) {
-    this.init(server);
+    this.init(server)
   }
 
   init(server) {
     // 创建实例
-    this.ws = new WebSocketServer({ server, path: "/node-pty" });
+    this.ws = new WebSocketServer({ server, path: '/node-pty' })
 
-    this.ws.on("connection", (socket, request) => {
-      if (!request.url.includes("/node-pty")) {
-        return socket.close();
+    this.ws.on('connection', (socket, request) => {
+      if (!request.url.includes('/node-pty')) {
+        return socket.close()
       }
-      this.online = this.ws._server._connections;
-      console.log(`socket当前在线${this.online}个连接`);
+      this.online = this.ws._server._connections
+      console.log(`socket当前在线${this.online}个连接`)
 
-      const query = qs.parse(request.url.split("?")[1] ?? "");
+      const query = qs.parse(request.url.split('?')[1] ?? '')
       if (!query?.id) {
-        return socket.close();
+        return socket.close()
       }
       // 记录 id
-      socket.id = query.id;
+      socket.id = query.id
 
-      const terminal = this.createTerminal(query);
+      const terminal = this.createTerminal(query)
 
       // socket.on 表示服务器接收一个客户端message 事件
-      socket.on("message", (data) => {
-        terminal.write(data);
-      });
+      socket.on('message', (data) => {
+        this.processMessageFromClient(query.id, data)
+      })
 
       // 客户端断开，自带事件
-      socket.on("disconnect", function () {
-        console.log("disconnect");
-        terminal.kill();
-      });
-    });
+      socket.on('disconnect', function () {
+        console.log('disconnect')
+        terminal.kill()
+      })
+    })
   }
 
   createTerminal({ cols, rows, id }) {
-    console.log(cols, rows);
+    console.log(cols, rows)
 
     const terminal = new Terminal([], {
       cols: Number(cols),
       rows: Number(rows),
-    });
+    })
 
     terminal.onData((data) => {
       // send data
-      this.sendToClient(id, data);
-    });
+      this.sendMessageToClient(id, 'input', data)
+    })
 
     terminal.onExit((exitCode, signal) => {
       // close socket
-      this.closeClient(id, exitCode);
-    });
+      this.closeClient(id, exitCode)
+    })
 
-    this.terminals[id] = terminal;
+    this.terminals[id] = terminal
 
-    return terminal;
+    return terminal
+  }
+
+  processMessageFromClient(id, message) {
+    const terminal = this.terminals[id]
+    try {
+      if (terminal) {
+        const data = JSON.parse(message.toString())
+        console.log('receive message from client', data)
+        switch (data?.type) {
+          case 'input':
+            console.log('write to terminal', data?.content)
+            terminal.write(data?.content ?? '')
+            break
+          case 'heartbeat':
+            this.sendMessageToClient(id, 'heartbeat')
+            break
+          case 'resize':
+            const { cols, rows } = data?.content ?? {}
+            terminal.resize(cols, rows)
+            break
+        }
+      }
+    } catch (e) {
+      console.log(`process message from ${id} error`, e)
+    }
   }
 
   // 发送客户端数据
-  sendToClient(id, data) {
-    const socket = [...this.ws.clients].find((client) => client.id === id);
-    if (socket?.readyState === OPEN) {
-      socket.send(data);
+  sendMessageToClient(id, type, data) {
+    const socket = this.getSocketById(id)
+    if (this.isSocketReady(socket)) {
+      switch (type) {
+        case 'input':
+          socket.send(JSON.stringify({ type: 'input', content: data }))
+          break
+        case 'heartbeat':
+          socket.send(JSON.stringify({ type: 'heartbeat' }))
+          break
+      }
     }
   }
 
   closeClient(id, exitCode) {
-    const socket = [...this.ws.clients].find((client) => client.id === id);
-    socket.close(1006, `exit code ${exitCode}`);
+    const socket = this.getSocketById(id)
+    socket?.close(1006, `exit code ${exitCode}`)
+  }
+
+  getSocketById(id) {
+    return [...this.ws.clients].find((client) => client.id === id)
+  }
+
+  isSocketReady(socket) {
+    return socket?.readyState === OPEN
   }
 }
 
-module.exports = PtyWs;
+module.exports = PtyWs
