a19a975d7f7deaac718a43e44418917b7dd26e92
destiny <1848765519@qq.com>
2023-04-21 16:30:16 +0800

    chore: add prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
