3da6e0390fe812d718a41439241ecb2589ed135b
destiny <1848765519@qq.com>
2024-04-30 19:01:35 +0800

    feat(terminal): 默认配置增加类型定义

--- packages/terminal/src/core/config.ts
+++ packages/terminal/src/core/config.ts
@@ -1,14 +1,21 @@
+import { ISearchOptions } from '@xterm/addon-search'
+import { ITerminalOptions, ITheme } from '@xterm/xterm'
 // 默认主题配置
-export const DEFAULT_THEME = {}
+// https://github.com/xtermjs/xterm.js/blob/5.3.0/typings/xterm.d.ts#L323
+export const DEFAULT_THEME: ITheme = {
+  selectionBackground: '#2e5b8c',
+}
 
 // xterm 默认配置
-export const DEFAULT_XTERM_OPTIONS = {
+// https://github.com/xtermjs/xterm.js/blob/5.3.0/typings/xterm.d.ts#L26
+export const DEFAULT_XTERM_OPTIONS: ITerminalOptions = {
   cursorBlink: true,
   allowProposedApi: true,
   theme: DEFAULT_THEME,
 }
 
 // 搜索默认配置
-export const DEFAULT_SEARCH_OPTIONS = {
+// https://github.com/xtermjs/xterm.js/blob/5.3.0/addons/xterm-addon-search/src/SearchAddon.ts#L10
+export const DEFAULT_SEARCH_OPTIONS: ISearchOptions = {
   incremental: true,
 }
